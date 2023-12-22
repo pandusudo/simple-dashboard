a08dce7c0f8b45f08658b12546307fe2971cb67a
pandusudo <pandusudo@gmail.com>
2023-12-22 20:59:10 +0700

    fix cookie configs

--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -3,9 +3,6 @@ import { CookieOptions } from 'express';
 export const cookieSettings: CookieOptions = {
   httpOnly: true,
   secure: process.env.ENVIRONMENT !== 'development',
-  sameSite: 'strict',
-  domain:
-    process.env.ENVIRONMENT !== 'development'
-      ? process.env.FE_DOMAIN
-      : '.localhost',
+  sameSite: 'lax',
+  domain: process.env.FE_DOMAIN,
 };
