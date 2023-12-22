2634495f60a97de7e26341ce0dbaf8bda0d0a9dd
pandusudo <pandusudo@gmail.com>
2023-12-22 20:27:37 +0700

    add fe domain for cookie

--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -4,4 +4,8 @@ export const cookieSettings: CookieOptions = {
   httpOnly: true,
   secure: process.env.ENVIRONMENT !== 'development',
   sameSite: 'strict',
+  domain:
+    process.env.ENVIRONMENT !== 'development'
+      ? process.env.FE_DOMAIN
+      : '.localhost',
 };
