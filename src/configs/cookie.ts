28933075bd8dd5458ee1e06181922e691ea65b22
pandusudo <pandusudo@gmail.com>
2023-12-22 21:49:53 +0700

    fix same site config

--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -3,6 +3,6 @@ import { CookieOptions } from 'express';
 export const cookieSettings: CookieOptions = {
   httpOnly: true,
   secure: process.env.ENVIRONMENT !== 'development',
-  sameSite: 'lax',
+  sameSite: 'strict',
   domain: process.env.FE_DOMAIN,
 };
