6ab0a26f0f9ffe0e545a1d87715e8692ef4fc7dd
pandusudo <pandusudo@gmail.com>
2023-12-24 23:00:05 +0700

    add max age for cookie

--- src/configs/cookie.ts
+++ src/configs/cookie.ts
@@ -5,4 +5,5 @@ export const cookieSettings: CookieOptions = {
   secure: process.env.ENVIRONMENT !== 'development',
   sameSite: 'strict',
   domain: process.env.FE_DOMAIN,
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 };
